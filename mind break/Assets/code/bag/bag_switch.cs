23abe9cc12977495822189f713450ff92048884e
pudding1524 <66768318+pudding1524@users.noreply.github.com>
2021-10-29 01:29:44 +0800

    Bag_manger
    背包尚未完工

---
+++ mind break/Assets/code/bag/bag_switch.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class bag_switch : MonoBehaviour
+{
+    public GameObject bag;
+    //public GameObject Bag_Canvas;
+    public GameObject _select;
+    [SerializeField] private Button button;
+
+    private void Start()
+    {
+        _select.GetComponent<Button>().Select();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            bag.SetActive(true);
+            Time.timeScale = 0f;
+            Canvas_Open();
+        }
+    }
+
+    private void Canvas_Open()
+    {
+        //button = bag.transform.GetChild(0).GetComponent<Button>();
+        //button.Select();
+    }
+}
