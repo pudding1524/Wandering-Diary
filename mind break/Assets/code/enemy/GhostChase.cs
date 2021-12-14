80b1a28e810e5126b008f40f697655810d480dcf
pudding1524 <66768318+pudding1524@users.noreply.github.com>
2021-12-14 11:58:56 +0800

    AI優化
    刪減movepos=max , min

--- mind break/Assets/code/enemy/GhostChase.cs
+++ mind break/Assets/code/enemy/GhostChase.cs
@@ -7,14 +7,22 @@ using Random = UnityEngine.Random;
 
 public class GhostChase : MonoBehaviour
 {
+    public enum Enemy_Speed
+    {
+        Speed_up,
+        Speed_normal
+    }
+    public Enemy_Speed enemy_Speed;
+
 
     public float speed;
     public float startWaitTime;
-    private float waitTime;
+    public float waitTime;
 
     public Transform movePos;
     public Transform MaxPos;
     public Transform MinPos;
+    public Transform target;
 
     public GameObject gameover;
 
@@ -35,33 +43,65 @@ public class GhostChase : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, movePos.position, speed * Time.deltaTime);
 
-        if(Vector2.Distance(transform.position, movePos.position) < 0.1f)
+        transform.position = Vector2.MoveTowards(transform.position, movePos.position, speed * Time.deltaTime);
+        switch (enemy_Speed)
         {
-            if(waitTime <= 0)
-            {
-                movePos.position = GetRandomPos();
-                waitTime = startWaitTime;
-            }
-            else
-            {
-                waitTime -= Time.deltaTime;
-            }
+            case Enemy_Speed.Speed_normal:
+               
+                Speed_normal();
+                
+                if (Vector2.Distance(transform.position, movePos.position) < 0.1f)
+                {
+                    if (waitTime <= 0)
+                    {
+                        movePos.position = GetRandomPos();
+                        waitTime = startWaitTime;
+                        
+                    }
+                    else
+                    {
+                        waitTime -= Time.deltaTime;
+                    }
+                }
+                way_();
+
+                break;
+
+            case Enemy_Speed.Speed_up:
+                Speed_Change_Up();
+
+                if (Vector2.Distance(transform.position, movePos.position) < 0.1f)
+                {
+                    if (waitTime <= 0)
+                    {
+                        movePos.position = GetRandomPos();
+                        waitTime = startWaitTime;
+
+                    }
+                    else
+                    {
+                        waitTime -= Time.deltaTime;
+                    }
+                }
+                way_();
+                break;
         }
-        way_();
+        
+        
         
 
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            gameover.SetActive(true);
-            Time.timeScale = 0f;
-        }   
-    }
+
+    //private void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    if (other.gameObject.CompareTag("Player"))
+    //    {
+    //        gameover.SetActive(true);
+    //        Time.timeScale = 0f;
+    //    }   
+    //}
 
     Vector2 GetRandomPos()
     {
@@ -83,21 +123,17 @@ public class GhostChase : MonoBehaviour
         }
     }
 
+
     public void Speed_Change_Up()
     {
         speed = 10;
-        if(transform.position.x < movePos.position.x)
-        {
-            movePos = MinPos;
-        }
-        else
-        {
-            movePos = MaxPos;
-        }
+        enemy_Speed = Enemy_Speed.Speed_up;
+        
     }
 
     public void Speed_normal()
     {
         speed = 5;
+        enemy_Speed = Enemy_Speed.Speed_normal;
     }
 }
